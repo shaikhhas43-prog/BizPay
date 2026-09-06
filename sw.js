2b94e267e5d93050d79a480e9ecc9b77cdae1a46
shaikhhas43-prog <shaikhhas43@gmail.com>
2026-09-06 14:51:31 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'bizpay-pwa-v1';
+const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      if (new URL(req.url).origin === location.origin) caches.open(CACHE).then(c => c.put(req, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
